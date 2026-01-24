319d9cc9872e8eb7d4d7bce2ea11068d579012e3
Vidar Magnusson <git@vidarmagnusson.com>
2026-01-24 09:06:19 +0100

    TEMP

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -1,9 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
-use std::net::IpAddr;
+use std::{convert::Infallible, net::IpAddr};
 
-use rocket::launch;
+use rocket::{Request, http::Status, launch, outcome::Outcome, request::FromRequest};
 
 #[launch]
 fn rocket() -> _ {
@@ -18,4 +18,28 @@ fn get_ip(ip_addr: IpAddr) -> String {
     let ip_type = if ip_addr.is_ipv4() { "IPv4" } else { "IPv6" };
 
     format!("{ip_type} {}", ip_addr.to_string())
+}
+
+#[get("/debug/ip")]
+fn debug_ip(ips: DebugIps) -> String {
+    ips.data
+}
+
+struct DebugIps {
+    data: String,
+}
+
+#[crate::async_trait]
+impl<'r> FromRequest<'r> for DebugIps {
+    type Error = Infallible;
+
+    async fn from_request(
+        request: &'r Request<'_>,
+    ) -> Outcome<Self, (Status, Self::Error), Status> {
+        let real_ip = request.headers().get("X-Real-IP").collect::<Vec<_>>();
+        let forwarded_for = request.headers().get("X-Forwarded-For").collect::<Vec<_>>();
+
+        let data = String::new();
+        Outcome::Success(DebugIps { data })
+    }
 }
